77c05af513edcb054b8543d452f7e849787e1ce7
luisuribemoreira <luisuribemoreira@gmail.com>
2018-10-01 12:11:13 -0300

    Inicio del sistema
    En este commit se subio la primera version de la aplicacion de escritorio, con la conexion a la base de datos y una primera version del login.

---
+++ MisOfertas/src/MisPaquetes/Conexion.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MisPaquetes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class Conexion {
+    
+    private Connection conexion_base;
+
+    public Connection getConexion_base() {
+        return conexion_base;
+    }
+
+    public void setConexion_base(Connection conexion_base) {
+        this.conexion_base = conexion_base;
+    }
+    
+    public Conexion Conectar() throws ClassNotFoundException, SQLException{
+        Class.forName("oracle.jdbc.OracleDriver");
+        String url = "jdbc:oracle:thin:@localhost:1521:XE";
+        conexion_base = DriverManager.getConnection(url, "MISOFERTAS", "MOS");
+        return this;
+    }
+}
---
+++ MisOfertas/src/Sistema/MainSistema.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sistema;
+
+/**
+ *
+ * @author Nicolás
+ */
+public class MainSistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SistemaMisOfertas sistema = new SistemaMisOfertas();
+        sistema.setVisible(true);
+    }
+    
+}
